0aeebe1ac1923e931029568beebef9971c11de35
a1eksey.Vorovyev <a1eksey@rambler.ru>
2023-03-02 14:36:12 +0300

    C++ programm

--- lesson05/hello.c
+++ lesson05/hello.c
@@ -2,13 +2,23 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 using namespace std;
+
+bool isDigit(string text){
+	for(int i=0;i<text.length();i++){
+		if (text[i]<48 and text[i]>57)
+			return false;
+	}
+	return true;
+}
 
 int main (void)
 {
 	//srand(time(0));
 	int a,cnt,seachNumber;
+	string text;
 	cnt = 5;
 	srand(unsigned(time(0)));
 	seachNumber = rand()%100;
@@ -18,18 +28,30 @@ int main (void)
 	while (cnt>0)
 	{
 		printf ("Введите ваше число\n");
-		scanf("%d", &a); // ввод  переменной a с клавиатуры
-		if (seachNumber==a){
-			printf ("Вы угадали число\n");
-			break;
-		}
-		if (seachNumber>a){
+		//scanf("%d", &a); // ввод  переменной a с клавиатуры
+		cin>>text;
+		if (isDigit(text)){
+			try{
+				a = stoi(text);
+				if (seachNumber==a){
+					printf ("Вы угадали число\n");
+					break;
+				}
+				if (seachNumber>a){
                         printf ("Больше.\n");
-                }
-		if (seachNumber<a){
+                	}
+				if (seachNumber<a){
                         printf ("Меньше.\n");
-                }
-		cnt--;
+                	}
+				cnt--;
+			}catch(...){
+				printf ("Вводите только числа\n");
+			}
+		}
+		else {
+			printf ("Вводите только числа\n");
+			continue;
+		}
 	}
 	if (seachNumber!=a)
 		 printf("У вас закончились попытки. Попробуйте еще раз.\n");
