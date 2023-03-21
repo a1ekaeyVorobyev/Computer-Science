515d7ed31aeec0b9c679de0f731ea75b1c022b2e
a1eksey.Vorovyev <a1eksey@rambler.ru>
2023-03-21 11:03:17 +0300

    test

---
+++ lesson06/test/main.c
@@ -0,0 +1,13 @@
+#include "test.h" // Везде подключаем только заголовочные файлы!
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    val1 = 10;
+    val2 = 15;
+    printf("val1 = %d\n", val1);
+    printf("val2 = %d\n", val2);
+    printf("echo(10)");
+    echo(10);
+    return 0;
+}
---
+++ lesson06/test/test.c
@@ -0,0 +1,8 @@
+#include "test.h"
+#include <stdio.h>
+
+void echo(int a) {
+    printf("val1 = %d\n", val1);
+    printf("val2 = %d\n", val2);
+    printf("%d\n", a);
+}
