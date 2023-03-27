7c00563dbc6767291ef4b038f55f08f16ccadca6
a1eksey.Vorovyev <a1eksey@rambler.ru>
2023-03-27 23:14:37 +0300

    chat server

---
+++ lesson07/client.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(int argc, char *argv[])
+{
+	const char *port = "8088";		/* порт по умолчанию */
+	char *host;
+	struct addrinfo hints,*server;
+	int r,sockfd,done;
+	char buffer[BUFSIZ];
+	fd_set read_fd;
+
+	if( argc<2 )
+	{
+		fprintf(stderr,"Задайте адрес chat сервера.\n");
+		exit(1);
+	}
+	host = argv[1];
+    if (argc>2)
+        port = argv[2];
+    //printf("host = %s\n",host);
+    //printf("Порт = %s\n",port);
+
+	printf("Looking for chat server on %s...",host);
+	memset( &hints, 0, sizeof(hints) );		// use memset_s() 
+	hints.ai_family = AF_INET;				// IPv4 
+	hints.ai_socktype = SOCK_STREAM;		// TCP 
+	r = getaddrinfo( host, port, &hints, &server );
+	if( r!=0 )
+	{
+		perror("failed");
+		exit(1);
+	}
+	puts("found");
+
+	// создаем socket 
+	sockfd = socket(server->ai_family,server->ai_socktype,server->ai_protocol);
+	if( sockfd==-1 )
+	{
+		perror("failed");
+		exit(1);
+	}
+
+	// соединение to the socket 
+	r = connect(sockfd,server->ai_addr,server->ai_addrlen);
+	freeaddrinfo(server);
+	if( r==-1 )
+	{
+		perror("failed");
+		exit(1);
+	}
+
+	// цикл для опроса дискирипторов 
+	done = FALSE;
+	while(!done)
+	{
+		// инициализируем   descriptor  
+		FD_ZERO(&read_fd);
+		FD_SET(sockfd, &read_fd);		// добавляем soccet 
+
+		FD_SET(0, &read_fd);			// добавляем стандартный input
+		r = select( sockfd+1, &read_fd, NULL, NULL, 0);
+		if( r==-1 )
+		{
+			perror("failed");
+			exit(1);
+		}
+
+		// получение из сервера
+		if( FD_ISSET(sockfd, &read_fd) )
+		{
+			r = recv( sockfd, buffer, BUFSIZ, 0);
+			if( r<1 )
+			{
+				puts("Connection closed by peer");
+				break;
+			}
+			// увеличение буфера 
+			buffer[r] = '\0';
+			printf("%s",buffer);
+		}
+
+		// ввод 
+        if( FD_ISSET(0, &read_fd) )
+		{
+			// Проверка на пустую строку
+			if( fgets(buffer,BUFSIZ,stdin)==NULL )
+			{
+				putchar('\n');
+			}
+			// выход из цикла и чата 
+			else if( strcmp(buffer,"close\n")==0 )
+			{
+				done=TRUE;
+			}
+			else
+			{
+				send(sockfd,buffer,strlen(buffer),0);
+			}
+		}
+	}	
+
+	printf("Disconnected\nДосвидание!\n");
+	close(sockfd);
+
+	return(0);
+}
---
+++ lesson07/server.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(int argc, char *argv[])
+{
+
+	char *port = "8088";					// порт для кликетов 
+    if (argc>1)
+        port = argv[1];
+    //printf("Порт = %s\n",port);
+	const int clientname_size = 32;				// для  клиентов IPv4 address
+	char clientname[clientname_size];
+	char buffer[BUFSIZ],sendstr[BUFSIZ];
+	const int backlog = 10;						// количество max соединений
+	char connection[backlog][clientname_size];	// сохрание  всех IPv4 сщужинений
+	socklen_t address_len = sizeof(struct sockaddr);
+	struct addrinfo hints,*server;
+	struct sockaddr address;
+	int r,max_connect,fd,x,done;
+	fd_set main_fd,read_fd;
+	int serverfd,clientfd;
+
+	/* setup the server */
+	memset( &hints, 0, sizeof(struct addrinfo));	
+	hints.ai_family = AF_INET;			// IPv4 
+	hints.ai_socktype = SOCK_STREAM;	// TCP 
+	hints.ai_flags = AI_PASSIVE;		// принять любое соединение
+	r = getaddrinfo( 0, port, &hints, &server );
+	if( r!=0 )
+	{
+		perror("failed");
+		exit(1);
+	}
+
+	/* create a socket */
+	serverfd = socket(server->ai_family,server->ai_socktype,server->ai_protocol);
+	if( serverfd==-1 )
+	{
+		perror("failed");
+		exit(1);
+	}
+
+	r = bind(serverfd,server->ai_addr,server->ai_addrlen);
+	if( r==-1 )
+	{
+		perror("failed");
+		exit(1);
+	}
+
+	// Ckeiftv cоединения
+	puts("Chat Server is listening...");
+	r = listen(serverfd,backlog);
+	if( r==-1 )
+	{
+		perror("failed");
+		exit(1);
+	}
+
+
+	max_connect = backlog;		
+	FD_ZERO(&main_fd);			// // инициализируем   descriptor  
+	FD_SET(serverfd, &main_fd);	
+
+	// Цикл для установки соединения
+	done = FALSE;
+	while(!done)
+	{
+
+		read_fd = main_fd;
+		
+		r = select(max_connect+1,&read_fd, NULL, NULL, 0);
+		if( r==-1 )
+		{
+			perror("failed");
+			exit(1);
+		}
+
+		//Для активный соединений.
+		for( fd=1; fd<=max_connect; fd++)
+		{
+			
+			if( FD_ISSET(fd,&read_fd) )
+			{
+				//Проверка на новые соединеия
+				if( fd==serverfd )
+				{
+					// Добавляем нового клиента
+					clientfd = accept(
+							serverfd,
+							(struct sockaddr *)&address,
+							&address_len
+							);
+					if( clientfd==-1 )
+					{
+						perror("failed");
+						exit(1);
+					}
+					
+					r = getnameinfo(
+							(struct sockaddr *)&address,
+							address_len,
+							clientname,
+							clientname_size,
+							0,
+							0,
+							NI_NUMERICHOST
+							);
+					// Добавляем в массив новое соединение
+					strcpy(connection[clientfd],clientname);
+
+					// Добавляем новое соединение в дискриптор
+					FD_SET(clientfd, &main_fd);
+
+
+					strcpy(buffer,"SERVER> Welcome ");
+					strcat(buffer,connection[clientfd]);
+					strcat(buffer," to the chat server\n");
+					strcat(buffer,"SERVER> Type 'close' to disconnect; 'shutdown' to stop\n");
+					send(clientfd,buffer,strlen(buffer),0);
+
+					
+					strcpy(buffer,"SERVER> ");
+					strcat(buffer,connection[clientfd]);
+					strcat(buffer," has joined the server\n");
+
+					for( x=serverfd+1; x<max_connect; x++ )
+					{
+						if( FD_ISSET(x,&main_fd) )
+							send(x,buffer,strlen(buffer),0);
+					}
+
+					printf("%s",buffer);
+				} 
+
+				else
+				{
+					r = recv(fd,buffer,BUFSIZ,0);
+					if( r<1 )
+					{
+						FD_CLR(fd, &main_fd);		
+						strcpy(buffer,"SERVER> ");
+						strcat(buffer,connection[fd]);
+						strcat(buffer," вышел.\n");
+
+						for( x=serverfd+1; x<max_connect; x++ )
+						{
+							if( FD_ISSET(x,&main_fd) )
+							{
+								send(x,buffer,strlen(buffer),0);
+							}
+						}
+
+						printf("%s",buffer);
+					}
+
+					else
+					{
+						buffer[r] = '\0';			
+						if( strcmp(buffer,"shutdown\n")==0 )
+						{
+							done = TRUE;		
+						}
+						
+						else
+						{
+	
+							strcpy(sendstr,connection[fd]);
+							strcat(sendstr,"> ");
+							strcat(sendstr,buffer);
+
+							for( x=serverfd+1; x<max_connect; x++ )
+							{
+
+								if( FD_ISSET(x,&main_fd) )
+								{
+									//отправка строки
+									send(x,sendstr,strlen(sendstr),0);
+								}
+							}
+
+							printf("%s",sendstr);
+						}	
+					}	
+				} 
+			} 
+		} 
+	} 
+	puts("SERVER> Выход");
+	/* закрытие сокера и очистка памяти */
+	close(serverfd);
+	freeaddrinfo(server);
+	return(0);
+}
