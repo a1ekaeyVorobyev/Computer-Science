6346a2604ca17d6c58d29ca0416d078741b2db79
a1eksey.Vorovyev <a1eksey@rambler.ru>
2023-06-06 09:08:01 +0300

    change Homework

--- lesson29/app/routes.py
+++ lesson29/app/routes.py
@@ -9,24 +9,8 @@ import intrep
 def index():
     if request.method == "POST":
         # getting input with name = fname in HTML form
-        try:
-            script = request.form.get("script")
-            lex_disassembly = lexer.LexDisassembly()
-            tokens = lex_disassembly.create_list_tokens(script)
-            str_token = '\n'.join(map(str, tokens))
-            interpretator = intrep.Interpretator()
-            for token in tokens:
-                interpretator.intrep(token)
-
-            return render_template(
-                "indexResponse.html",
-                title="Main Page",
-                script=script,
-                token=str_token,
-                result=interpretator.get_result(),
-            )
-        except:
-            return 'Не удалось выполнить скрипт.'
+        script = request.form.get("script")
+        return generate_result(script.strip())
     return render_template(
         "index.html",
         title="Main Page",
@@ -41,9 +25,12 @@ while a<100:
 @app.route('/exec')
 def query_example():
     # if key doesn't exist, returns None
+    script = request.args.get('query')
+    return generate_result(script.strip())
+
+def  generate_result(script:str):
+    """генерация страницы с результатом"""
     try:
-        script = request.args.get('query')
-        script = script.strip()
         lex_disassembly = lexer.LexDisassembly()
         tokens = lex_disassembly.create_list_tokens(script)
         str_token = '\n'.join(map(str, tokens))
