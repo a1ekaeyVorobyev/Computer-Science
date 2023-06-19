63cba21ae6ee66ba87ea061e368aeeeab08171b6
a1eksey.Vorovyev <a1eksey@rambler.ru>
2023-06-19 11:52:55 +0300

    action

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,5 +1,15 @@
-from flask import flash, redirect, render_template,request
+from flask import flash, redirect, render_template,request, jsonify
 from app import app
+from flask import Flask
+
+def validate_post_data(data: dict) -> bool:
+    if not isinstance(data, dict):
+        return False
+    if not data.get('name') or not isinstance(data['name'], str):
+        return False
+    if data.get('age') and not isinstance(data['age'], int):
+        return False
+    return True
 
 @app.route('/')
 @app.route("/index", methods=['GET', 'POST'])
@@ -17,6 +27,10 @@ def index():
     )
 
 
+@app.route('/test', methods=['GET'])
+def hello():
+    return 'Hello World!'
+
 @app.route("/page")
 def page2():
 
@@ -25,3 +39,22 @@ def page2():
         title="Second page",
         page="second", 
     )
+
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    """
+    /api entpoint
+    GET - returns json= {'status': 'test'}
+    POST -  {
+            name - str not null
+            age - int optional
+            }
+    :return:
+    """
+    if request.method == 'GET':
+        return jsonify({'status': 'test'})
+    elif request.method == 'POST':
+        if validate_post_data(request.json):
+            return jsonify({'status': 'OK'})
+        else:
+            return jsonify({'status': 'bad input'}), 400
